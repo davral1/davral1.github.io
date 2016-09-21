be865d5e6217880a09d171471ca7cc90b7db1d4a
davral1 <davral1@gmail.com>
2016-09-21 13:10:23 +0100

    f

--- php/callProxy.php
+++ php/callProxy.php
@@ -1,5 +1,6 @@
 <?php
-include '{your path here}\cartoDBProxy.php';
+header("Access-Control-Allow-Origin: *");
+include '/Users/DMAN/Documents/davral1.github.io/php/cartoDBProxy.php';
 //			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $queryURL = $_POST['qurl'];
 $return = goProxy($queryURL);
--- php/cartoDBProxy.php
+++ php/cartoDBProxy.php
@@ -1,11 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 session_cache_limiter('nocache');
 $cache_limiter = session_cache_limiter();
 function goProxy($dataURL) 
 {
-	$baseURL = 'http://{your CartoDB username}.cartodb.com/api/v2/sql?';
+	$baseURL = 'http://davral1.cartodb.com/api/v2/sql?';
 	//  					^ CHANGE THE 'CARTODB-USER-NAME' to your cartoDB url!
-	$api = '&api_key={your CartoDB API key}';
+	$api = '&api_key=4d7645acd6e06c6b8d03958b88b14d4e70326f40';
 	//				 ^ENTER YOUR API KEY HERE!
 	$url = $baseURL.'q='.urlencode($dataURL).$api;
 	$result = file_get_contents ($url);
