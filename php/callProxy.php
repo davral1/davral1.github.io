a37b21d2914a7fb7234065f4d66bde2cb8000762
davral1 <davral1@gmail.com>
2016-09-23 10:23:40 +0100

    w

---
+++ php/callProxy.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+include '{your path here}\cartoDBProxy.php';
+//			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
+$queryURL = $_POST['qurl'];
+$return = goProxy($queryURL);
+echo $return;
+?>
